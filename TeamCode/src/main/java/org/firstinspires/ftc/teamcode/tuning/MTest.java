bed59fa24edd935552f49cf6750b9cb1306580c4
janrmartin1 <46615567+janrmartin1@users.noreply.github.com>
2023-12-28 15:16:59 -0500

    tuning files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tuning/MTest.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.tuning;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+import org.firstinspires.ftc.teamcode.TankDrive;
+@Autonomous
+public final class MTest extends LinearOpMode {
+    public void runOpMode() throws InterruptedException {
+        if (TuningOpModes.DRIVE_CLASS.equals(MecanumDrive.class)) {
+            MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+            waitForStart();
+//I added the code between these comments
+            Actions.runBlocking(
+                    drive.actionBuilder(new Pose2d(0, 0, 0))
+                            .lineToX(10)
+                            .turnTo(Math.toRadians(90))
+                            .lineToY(10)
+                            .strafeTo(new Vector2d(10,0))
+                            .lineToX(0)
+                            .strafeTo(new Vector2d(10, 10))
+                            .splineTo(new Vector2d(10, 10), Math.PI)
+                            .build());
+
+//I added the code between these comments
+            Actions.runBlocking(
+                    drive.actionBuilder(drive.pose)
+                            .splineTo(new Vector2d(30, 30), Math.PI / 2)
+                            .splineTo(new Vector2d(60, 0), Math.PI)
+                            .build());
+        } else if (TuningOpModes.DRIVE_CLASS.equals(TankDrive.class)) {
+            TankDrive drive = new TankDrive(hardwareMap, new Pose2d(0, 0, 0));
+
+            waitForStart();
+
+            Actions.runBlocking(
+                    drive.actionBuilder(drive.pose)
+                            .splineTo(new Vector2d(30, 30), Math.PI / 2)
+                            .splineTo(new Vector2d(60, 0), Math.PI)
+                            .build());
+        } else {
+            throw new AssertionError();
+        }
+    }
+}
